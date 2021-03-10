ba81b54fb14e645e1ed83aeea88c73479f5b5f74
Eeshwar Krishnan <eeshwar.krishnan@gmail.com>
2021-03-10 17:12:42 -0500

    Updated OpenCV to 6.2, added variable segments to spline approximation

--- TeamCode/src/main/java/MathSystems/MathUtils.java
+++ TeamCode/src/main/java/MathSystems/MathUtils.java
@@ -13,8 +13,13 @@ public class MathUtils {
         double theta = Math.atan2(y, x);
         return new Vector2(r, theta);
     }
-
+    
     public static double getRadRotDist(double start, double end){
+        double dist = (end - start + Math.PI) % (Math.PI*2) - Math.PI;
+        return (dist < -Math.PI) ? dist + (2 * Math.PI) : dist;
+    }
+    
+    public static double getRadRotDiste(double start, double end){
         double dist = end - start;
         if(Math.abs(dist) > Math.PI){
             if(start > Math.PI){
@@ -25,101 +30,110 @@ public class MathUtils {
         }
         return dist;
     }
-
+    
     public static double sign(double in){
-        if(in == 0) {
-            return 1;
-        }
+    	if(in == 0) {
+    		return 1;
+    	}
         return in/Math.abs(in);
     }
-
+    
     public static ArrayList<Vector2> approxCurve(Vector2 start, Vector2 end, Vector2 control, double numSegments){
-        ArrayList<Vector2> coordinates = new ArrayList<Vector2>();
-
-        coordinates.add(start);
-
+    	ArrayList<Vector2> coordinates = new ArrayList<Vector2>();
+    	
+    	coordinates.add(start);
+    	
         double s  = 0;
         double t = 1;
 
         while (s < t) {
-            s += 1/numSegments;
-            double controlParameter = (1 - s);
-            Vector2 Q_0 = new Vector2(controlParameter * start.getA(), controlParameter * start.getB()).add(new Vector2(s * control.getA(), s * control.getB()));
-            Vector2 Q_1 = new Vector2(controlParameter * control.getA(), controlParameter * control.getB()).add(new Vector2(s * end.getA(), s * end.getB()));
-            Vector2 R_0 = new Vector2(controlParameter * Q_0.getA(), controlParameter * Q_0.getB()).add(new Vector2(s * Q_1.getA(), s * Q_1.getB()));
-            coordinates.add(R_0);
+          s += 1/numSegments; 
+          double controlParameter = (1 - s);
+          Vector2 Q_0 = new Vector2(controlParameter * start.getA(), controlParameter * start.getB()).add(new Vector2(s * control.getA(), s * control.getB()));          
+          Vector2 Q_1 = new Vector2(controlParameter * control.getA(), controlParameter * control.getB()).add(new Vector2(s * end.getA(), s * end.getB()));
+          Vector2 R_0 = new Vector2(controlParameter * Q_0.getA(), controlParameter * Q_0.getB()).add(new Vector2(s * Q_1.getA(), s * Q_1.getB()));
+          coordinates.add(R_0);
         }
         coordinates.remove(coordinates.size()-1);
-        coordinates.add(end);
+        //coordinates.add(end);
         return coordinates;
     }
-
+    
+    public static ArrayList<Vector2> approxCurve(Vector2 start, Vector2 end, Vector2 control){
+    	return approxCurve(start, end, control, (start.distanceTo(control) + control.distanceTo(end)) * 0.1);
+    }
+    
     public static Vector2 getClosestPoint(Vector2 line1point1, Vector2 line1point2, Vector2 line2point){
         double dx = line1point1.getA() - line1point2.getA();
         double dy = line1point1.getB() - line1point2.getB();
 
         double line1slope = 0;
-
+        
         double x = 0;
         double y = 0;
-
+        
         if(dx == 0 && dy == 0){
             return line1point1;
         }else if(dx == 0){
-            x = line1point1.getA();
-            y = line2point.getB();
+        	x = line1point1.getA();
+        	y = line2point.getB();
             //return new Vector2(line1point1.getA(), line2point.getB());
         }else if(dy == 0){
-            x = line2point.getA();
-            y = line1point1.getB();
+        	x = line2point.getA();
+        	y = line1point1.getB();
             //return new Vector2(line2point.getA(), line1point1.getB());
         }else{
             line1slope = dy/dx;
-
+            
             double line2slope = -1/line1slope;
-
+            
             x = ((-line2slope * line2point.getA()) + line2point.getB() + (line1slope * line1point1.getA()) - line1point1.getB())/(line1slope - line2slope);
-
+            
             y = line1slope * (x - line1point1.getA()) + line1point1.getB();
         }
-
+        
         double minX = Math.min(line1point1.getA(), line1point2.getA());
         double maxX = Math.max(line1point1.getA(), line1point2.getA());
-
+        
         double minY = Math.min(line1point1.getB(), line1point2.getB());
         double maxY = Math.max(line1point1.getB(), line1point2.getB());
-
+        
         if(x < minX || x > maxX || y < minY || y > maxY) {
-            if(line1point1.distanceTo(line2point) < line1point2.distanceTo(line2point)) {
-                return line1point1;
-            }else {
-                return line1point2;
-            }
+        	if(line1point1.distanceTo(line2point) < line1point2.distanceTo(line2point)) {
+        		return line1point1;
+        	}else {
+        		return line1point2;
+        	}
         }
 
         return new Vector2(x, y);
     }
-
+    
     public static double arcLength(ArrayList<Vector2> lines) {
-        double dist = 0;
-        for(int i = 1; i < lines.size(); i ++) {
-            dist += lines.get(i-1).distanceTo(lines.get(i));
-        }
-        return dist;
+    	double dist = 0;
+    	for(int i = 1; i < lines.size(); i ++) {
+    		dist += lines.get(i-1).distanceTo(lines.get(i));
+    	}
+    	return dist;
     }
-
+    
     public static double arcLength(List<Vector2> lines) {
-        double dist = 0;
-        for(int i = 1; i < lines.size(); i ++) {
-            dist += lines.get(i-1).distanceTo(lines.get(i));
-        }
-        return dist;
+    	double dist = 0;
+    	for(int i = 1; i < lines.size(); i ++) {
+    		dist += lines.get(i-1).distanceTo(lines.get(i));
+    	}
+    	return dist;
+    }
+    
+    public static double normalizeRadAngle(double angle) {
+    	double tau = Math.PI * 2;
+    	return (((angle % tau) + tau) % tau);
     }
 
     public static Vector2 toPolar(Vector2 pos){
         return toPolar(pos.getA(), pos.getB());
     }
-
+    
     public static double clamp(double val, double min, double max){
         return Math.max(min, Math.min(max, val));
     }
